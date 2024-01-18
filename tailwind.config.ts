6e2db94e76910054785ceec369f6e9d39d223128
Yash Gupta <gyash21@gmail.com>
2024-01-19 00:05:19 +0530

    update- tailwind config completed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -58,6 +58,28 @@ backgroundImage: {
 "auth-light": "url('/assets/images/auth-light.png')",
 },
 
-
+screens: {
+  xs: "420px",
+  },
+  keyframes: {
+  "accordion-down": {
+  from: { height: 0 },
+  to: { height: "var(--radix-accordion-content-height)" },
+  },
+  "accordion-up": {
+  from: { height: "var(--radix-accordion-content-height)" },
+  to: { height: 0 },
+  },
+  },
+  animation: {
+  "accordion-down": "accordion-down 0.2s ease-out",
+  "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+};
   
-export default config
+
+
+
